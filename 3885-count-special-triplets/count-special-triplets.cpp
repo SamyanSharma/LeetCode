318472829342dd49ee0aed37b0166dbdf3408778
Samyan Sharma <74348483+SamyanSharma@users.noreply.github.com>
2025-12-15 22:18:46 +0530

    Time: 786 ms (54.98%) | Memory: 399.9 MB (55.03%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.cpp
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     int specialTriplets(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        for(int i: nums){
-            mp[i]++;
-        }
-        unordered_map<int, int> prefix;
         const int MOD = 1e9 + 7;
-        long long count = 0;
-        for(int i: nums){
-            int total = mp[2 * i];
-            int left = prefix[2*i];
-            int right = total - left;
-            if (i == 0){
-                right--;
-            }
-            prefix[i]++;
-            count = (count + left * 1LL * right) % MOD;
+        unordered_map<int, int> numCnt;
+        unordered_map<int, int> numPartialCnt;
+
+        numCnt.reserve(nums.size() * 2);
+        numPartialCnt.reserve(nums.size() * 2);
+
+        for (int v : nums) {
+            numCnt[v]++;
         }
-        return count;
+
+        int ans = 0;
+        for (int v : nums) {
+            int target = v * 2;
+            int lCnt = numPartialCnt[target];
+            numPartialCnt[v]++;
+            int rCnt = numCnt[target] - numPartialCnt[target];
+            ans = (ans + (lCnt * 1LL * rCnt % MOD)) % MOD;
+        }
+
+        return ans;
     }
 };
