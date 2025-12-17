bb7cdd2387b0f31ae10cd8f7b672db6a3e5ef6fb
Samyan Sharma <74348483+SamyanSharma@users.noreply.github.com>
2025-12-17 13:05:23 +0530

    Time: 1221 ms (9.20%) | Memory: 345.4 MB (20.08%) - LeetSync

--- 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.cpp
+++ 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.cpp
@@ -1,45 +1,36 @@
 class Solution {
 public:
-    long long solve(int i,  int state, vector<int>& prices, int k,
-                    vector<vector<vector<long long>>>& dp) {
-
-        if (k < 0) {
-            return -1e9;
-        }
-        if (i == prices.size()) {
-
-            if (state == 0) {
-                return 0;
-            }
-
-            return -1e9;
-        }
-        if (dp[i][k][state] != -1) {
-            return dp[i][k][state];
-        }
-
-        long long normal = -1e9;
-        long long shortSell = -1e9;
-        long long skip = solve(i + 1,  state, prices, k, dp);
-
-        if (state == 0) {
-            normal = -prices[i] + solve(i + 1, 1, prices, k, dp);
-            shortSell = prices[i] + solve(i + 1, 2, prices, k, dp);
-
-        }
-
-        else if (state == 1) {
-            normal = prices[i] + solve(i + 1, 0, prices, k - 1, dp);
-
-        } else {
-            shortSell = -prices[i] + solve(i + 1, 0, prices, k - 1, dp);
-        }
-
-        return dp[i][k][state] = max(normal, max(shortSell, skip));
-    }
     long long maximumProfit(vector<int>& prices, int k) {
         int n = prices.size();
-            vector<vector<vector<long long>>> dp(n, vector<vector<long long>>(k+1, vector<long long>(3, -1)));
-            return solve(0, 0, prices, k, dp);
+        vector<vector<vector<long long>>> memo(
+            n, vector<vector<long long>>(k + 1, vector<long long>(3, -1)));
+
+        function<long long(int, int, int)> dfs = [&](int i, int j,int state) -> long long {
+            if (j == 0) {
+                return 0;
+            }
+            if (i == 0) {
+                return state == 0 ? 0 : (state == 1 ? -prices[0] : prices[0]);
+            }
+            if (memo[i][j][state] != -1) {
+                return memo[i][j][state];
+            }
+
+            int p = prices[i];
+            long long res;
+            if (state == 0) {
+                res = max(dfs(i - 1, j, 0),
+                          max(dfs(i - 1, j, 1) + p, dfs(i - 1, j, 2) - p));
+            } else if (state == 1) {
+                res = max(dfs(i - 1, j, 1), dfs(i - 1, j - 1, 0) - p);
+            } else {
+                res = max(dfs(i - 1, j, 2), dfs(i - 1, j - 1, 0) + p);
+            }
+            memo[i][j][state] = res;
+
+            return res;
+        };
+
+        return dfs(n - 1, k, 0);
     }
 };
