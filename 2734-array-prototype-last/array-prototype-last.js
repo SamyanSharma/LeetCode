0c8043c860544fff8fd5fb0540e82037ccf53e9f
Samyan Sharma <74348483+SamyanSharma@users.noreply.github.com>
2025-12-25 22:23:28 +0530

    Time: 48 ms (34.97%) | Memory: 53.6 MB (70.50%) - LeetSync

--- 2734-array-prototype-last/array-prototype-last.js
+++ 2734-array-prototype-last/array-prototype-last.js
@@ -2,12 +2,8 @@
  * @return {null|boolean|number|string|Array|Object}
  */
 Array.prototype.last = function() {
-    if(this.length == 0){
-        return -1;
-    }
-    else{
-        return this[this.length-1];
-    }
+    if(this.length == 0) return -1;
+    return this[this.length - 1];
 };
 
 /**
