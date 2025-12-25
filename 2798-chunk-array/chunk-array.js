b095e71b0fddc79a0a9c0cbb33df47e9062b5175
Samyan Sharma <74348483+SamyanSharma@users.noreply.github.com>
2025-12-25 22:22:09 +0530

    Time: 40 ms (91.66%) | Memory: 56.2 MB (54.54%) - LeetSync

--- 2798-chunk-array/chunk-array.js
+++ 2798-chunk-array/chunk-array.js
@@ -7,7 +7,7 @@ var chunk = function(arr, size) {
     let arr1 = arr;
     let chunkArr = [];
     let result = [];
-    while(arr1.length!=0){
+    while (arr1.length != 0){
         chunkArr = arr1.splice(0, size);
         result.push(chunkArr);
     }
