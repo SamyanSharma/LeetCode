f4f9f6adb2cb7d42470927371119d40db6314ad2
Samyan Sharma <74348483+SamyanSharma@users.noreply.github.com>
2025-12-25 14:06:08 +0530

    Time: 159 ms (52.84%) | Memory: 107.9 MB (65.96%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
-        // Max heap by default
-        priority_queue<int> pq; 
-
-        // Push all the happiness values into the priority queue
-        for(const auto& h: happiness)
-            pq.push(h); 
-
-        long long totalHappinessSum = 0;
+        long long sum = 0;
         int turns = 0;
-
-        for(int i = 0; i < k; i++) {
-            // Add the current highest value to the total happiness sum
-            totalHappinessSum += max(pq.top() - turns, 0);
-
-            // Remove the highest value after using it
-            pq.pop(); 
-
-            // Increment turns for the next iteration
-            turns++; 
+        sort(happiness.begin(), happiness.end(), greater<int>());
+        for(int i=0; i<k; i++){
+            if(happiness[i]>turns){
+                sum += happiness[i] - turns;
+            }
+            turns++;
         }
-
-        return totalHappinessSum;
+        return sum;
     }
 };
