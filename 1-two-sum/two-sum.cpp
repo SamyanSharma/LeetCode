8e563c4be7840e5106add794fbf550292d56c429
Samyan Sharma <74348483+SamyanSharma@users.noreply.github.com>
2025-12-30 22:16:00 +0530

    Time: 4 ms (53.33%) | Memory: 14.9 MB (19.85%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,9 +4,7 @@ public:
         unordered_map <int, int> mp;
         for(int i = 0; i < nums.size(); i++){
             int diff = target - nums[i];
-            if(mp.find(diff) != mp.end()){
-                return {mp[diff], i};
-            }
+            if(mp.find(diff) != mp.end()) return {mp[diff], i};
             mp[nums[i]] = i;
         }
         return {};
