c0a953603340de6d22f28eb13d0fee4ff76b930b
Samyan Sharma <74348483+SamyanSharma@users.noreply.github.com>
2026-01-05 23:35:23 +0530

    Time: 0 ms (100.00%) | Memory: 39.1 MB (61.32%) - LeetSync

---
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        long long totalSum = 0;
+        int minVal = INT_MAX;
+        int negativeCount = 0;
+
+        for (auto& row : matrix){
+            for (int val : row){
+                totalSum += abs(val);
+                if (val < 0){
+                    negativeCount++;
+                }
+                minVal = min(minVal, abs(val));
+            }
+        }
+        if (negativeCount % 2 != 0){
+            totalSum -= 2 * minVal;
+        }
+        return totalSum;
+    }
+};
