5b6172f20f1e03396b63a95ac0765783a57da4a9
Samyan Sharma <74348483+SamyanSharma@users.noreply.github.com>
2026-07-22 13:37:47 +0530

    Time: 1 ms (94.40%) | Memory: 43 MB (71.24%) - LeetSync

--- 228-summary-ranges/summary-ranges.java
+++ 228-summary-ranges/summary-ranges.java
@@ -1,29 +1,18 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> result = new ArrayList<>();
-        if(nums == null || nums.length == 0){
-            return result;
-        }
-        int start = nums[0];
-        int end = nums[0];
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i] == nums[i - 1] + 1){
-                end = nums[i];
-            } else{
-                if(start == end){
-                    result.add(String.format("%d", start));
-                } else{
-                    result.add(String.format("%d->%d", start, end));
-                }
-                start = nums[i];
-                end = nums[i];
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            int start = nums[i];
+            while(i + 1< n && nums[i] + 1 == nums[i + 1]){
+                i++;
             }
-        }
-        if(start == end){
-            result.add(String.format("%d", start));
-        }
-        else{
-            result.add(String.format("%d->%d", start, end));
+            if(nums[i] == start){
+                result.add(String.valueOf(start));
+            }
+            else{
+                result.add(String.format("%d->%d", start, nums[i]));
+            }
         }
         return result;
     }
